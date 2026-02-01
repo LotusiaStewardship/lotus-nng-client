0aa6abd66da66d5a03ce5fa8b6b7ecfb10527aa4
maff1989 <matthew@urgero.net>
2026-02-01 00:03:08 -0700

    style(nng-interface): apply prettier formatting to FlatBuffers interfaces
    Run prettier on all generated TypeScript files in lib/nng-interface/
    to ensure consistent code style (semicolons, spacing, quotes).

--- lib/nng-interface/get-mempool-request.ts
+++ lib/nng-interface/get-mempool-request.ts
@@ -2,37 +2,53 @@
 
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any, @typescript-eslint/no-non-null-assertion */
 
-import * as flatbuffers from 'flatbuffers';
+import * as flatbuffers from 'flatbuffers'
 
 export class GetMempoolRequest {
-  bb: flatbuffers.ByteBuffer|null = null;
-  bb_pos = 0;
-  __init(i:number, bb:flatbuffers.ByteBuffer):GetMempoolRequest {
-  this.bb_pos = i;
-  this.bb = bb;
-  return this;
-}
+  bb: flatbuffers.ByteBuffer | null = null
+  bb_pos = 0
+  __init(i: number, bb: flatbuffers.ByteBuffer): GetMempoolRequest {
+    this.bb_pos = i
+    this.bb = bb
+    return this
+  }
 
-static getRootAsGetMempoolRequest(bb:flatbuffers.ByteBuffer, obj?:GetMempoolRequest):GetMempoolRequest {
-  return (obj || new GetMempoolRequest()).__init(bb.readInt32(bb.position()) + bb.position(), bb);
-}
+  static getRootAsGetMempoolRequest(
+    bb: flatbuffers.ByteBuffer,
+    obj?: GetMempoolRequest,
+  ): GetMempoolRequest {
+    return (obj || new GetMempoolRequest()).__init(
+      bb.readInt32(bb.position()) + bb.position(),
+      bb,
+    )
+  }
 
-static getSizePrefixedRootAsGetMempoolRequest(bb:flatbuffers.ByteBuffer, obj?:GetMempoolRequest):GetMempoolRequest {
-  bb.setPosition(bb.position() + flatbuffers.SIZE_PREFIX_LENGTH);
-  return (obj || new GetMempoolRequest()).__init(bb.readInt32(bb.position()) + bb.position(), bb);
-}
+  static getSizePrefixedRootAsGetMempoolRequest(
+    bb: flatbuffers.ByteBuffer,
+    obj?: GetMempoolRequest,
+  ): GetMempoolRequest {
+    bb.setPosition(bb.position() + flatbuffers.SIZE_PREFIX_LENGTH)
+    return (obj || new GetMempoolRequest()).__init(
+      bb.readInt32(bb.position()) + bb.position(),
+      bb,
+    )
+  }
 
-static startGetMempoolRequest(builder:flatbuffers.Builder) {
-  builder.startObject(0);
-}
+  static startGetMempoolRequest(builder: flatbuffers.Builder) {
+    builder.startObject(0)
+  }
 
-static endGetMempoolRequest(builder:flatbuffers.Builder):flatbuffers.Offset {
-  const offset = builder.endObject();
-  return offset;
-}
+  static endGetMempoolRequest(
+    builder: flatbuffers.Builder,
+  ): flatbuffers.Offset {
+    const offset = builder.endObject()
+    return offset
+  }
 
-static createGetMempoolRequest(builder:flatbuffers.Builder):flatbuffers.Offset {
-  GetMempoolRequest.startGetMempoolRequest(builder);
-  return GetMempoolRequest.endGetMempoolRequest(builder);
-}
+  static createGetMempoolRequest(
+    builder: flatbuffers.Builder,
+  ): flatbuffers.Offset {
+    GetMempoolRequest.startGetMempoolRequest(builder)
+    return GetMempoolRequest.endGetMempoolRequest(builder)
+  }
 }
